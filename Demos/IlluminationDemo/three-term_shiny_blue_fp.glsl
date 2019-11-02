b08c813dca006dffe5a29d10afc24bef3a9fc1b9
Shalin Lathigra <shalinlathigra@WINDOWS2.SCS.CARLETON.CA>
2019-11-02 16:37:17 -0400

    beginning assignment

--- Demos/IlluminationDemo/three-term_shiny_blue_fp.glsl
+++ Demos/IlluminationDemo/three-term_shiny_blue_fp.glsl
@@ -14,14 +14,19 @@ vec4 specular_color = vec4(0.8, 0.5, 0.9, 1.0);
 float phong_exponent = 64.0;
 float Ia = 0.2; // Ambient light amount
 
+// Global Lighting Attributes
+vec3 L_directional = normalize(vec3(1, 0, -1));
+vec4 directional_color = vec4(0.8, 0.5, 0.0, 1.0);
+float Da = 0.1; //Ambient Directional Amount
 
 void main() 
 {
     vec3 N, // Interpolated normal for fragment
          L, // Light-source direction
          V, // View direction
-         H; // Half-way vector
-
+         R; // Reflected vector
+		 
+	//LOCAL ILLUMINATION!!!!
     // Compute Lambertian term Id
     N = normalize(normal_interp);
 
@@ -34,17 +39,25 @@ void main()
     // V = (eye_position - position_interp);
     V = - position_interp; // Eye position is (0, 0, 0) in view coordinates
     V = normalize(V);
+	
+	//R = -L + 2(dot(L, N) * N
+	R = normalize(-L + 2(dot(L, N) * N);
 
-    //H = 0.5*(V + L); // Halfway vector
-    H = (V + L); // Halfway vector (will be normalized anyway)
-    H = normalize(H);
-
-    float spec_angle_cos = max(dot(N, H), 0.0);
+    float spec_angle_cos = max(dot(V, R), 0.0);
     float Is = pow(spec_angle_cos, phong_exponent);
         
     // Assign illumination to the fragment
     gl_FragColor = Ia*ambient_color + Id*diffuse_color + Is*specular_color;
                     
+
+	//GLOBAL ILLUMINATION!!!!
+	L = L_directional;
+	
+	//float Dd = ciel(max(dot(N, L), 0.0));
+	float Dd = max(dot(N, L), 0.0);
+
+	gl_FragColor = (Da + Dd)*directional_color;
+
     // For debug, we can display the different values
     //gl_FragColor = ambient_color;
     //gl_FragColor = diffuse_color;
