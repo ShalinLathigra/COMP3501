e9e466d30fa87eeb279494e1d1bb53b319842e90
Shalin Lathigra <shalinlathigra@windows2.scs.carleton.ca>
2019-11-04 08:46:55 -0500

    working Illumination Demo, with Phong specular

--- Demos/IlluminationDemo/three-term_shiny_blue_fp.glsl
+++ Demos/IlluminationDemo/three-term_shiny_blue_fp.glsl
@@ -14,19 +14,17 @@ vec4 specular_color = vec4(0.8, 0.5, 0.9, 1.0);
 float phong_exponent = 64.0;
 float Ia = 0.2; // Ambient light amount
 
-// Global Lighting Attributes
-vec3 L_directional = normalize(vec3(1, 0, -1));
-vec4 directional_color = vec4(0.8, 0.5, 0.0, 1.0);
-float Da = 0.1; //Ambient Directional Amount
+vec4 directional_color = vec4(1.0, 0.7, 0.0, 1.0);
+vec3 L_dir = normalize(vec3(1.0, -1.0, 1.0));
+float directional_intensity = .5f; //Added this value because I wanted control over how bright the light was, without having to mess with the colour vec4 too much
 
 void main() 
 {
     vec3 N, // Interpolated normal for fragment
          L, // Light-source direction
          V, // View direction
-         R; // Reflected vector
-		 
-	//LOCAL ILLUMINATION!!!!
+         R; // Reflected Light vector
+
     // Compute Lambertian term Id
     N = normalize(normal_interp);
 
@@ -39,25 +37,20 @@ void main()
     // V = (eye_position - position_interp);
     V = - position_interp; // Eye position is (0, 0, 0) in view coordinates
     V = normalize(V);
-	
-	//R = -L + 2(dot(L, N) * N
-	R = normalize(-L + 2(dot(L, N) * N);
 
-    float spec_angle_cos = max(dot(V, R), 0.0);
+    //R = -L + 2*(dot(N, L)*N); // Reflected Light Vector
+    R = -L + 2*(dot(N, L)*N); // Reflected Light Vector (will be normalized anyway)
+    R = normalize(R);
+
+    float spec_angle_cos = max(dot(N, R), 0.0);
     float Is = pow(spec_angle_cos, phong_exponent);
         
     // Assign illumination to the fragment
     gl_FragColor = Ia*ambient_color + Id*diffuse_color + Is*specular_color;
+
+	float Dd = max(dot(N, L_dir), 0.0);
+    gl_FragColor += Dd * directional_color * directional_intensity;
                     
-
-	//GLOBAL ILLUMINATION!!!!
-	L = L_directional;
-	
-	//float Dd = ciel(max(dot(N, L), 0.0));
-	float Dd = max(dot(N, L), 0.0);
-
-	gl_FragColor = (Da + Dd)*directional_color;
-
     // For debug, we can display the different values
     //gl_FragColor = ambient_color;
     //gl_FragColor = diffuse_color;
